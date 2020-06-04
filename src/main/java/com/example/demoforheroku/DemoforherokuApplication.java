c1875ad5879a802ea200d64758e4861495c9bdd3
Briano Bruno <briano.bruno@cix.csi.cuny.edu>
2020-06-03 21:03:12 -0400

    first commit

---
+++ src/main/java/com/example/demoforheroku/DemoforherokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoforheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoforherokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoforherokuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoforheroku/DemoforherokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoforheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoforherokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
